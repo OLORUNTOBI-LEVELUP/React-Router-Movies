a50bc964eaa50fa708812327e7cbdee58137ed8b
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-10 12:44:19 +0100

    Changed a line in movie.js to fetch the Id

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,8 @@ export default class App extends Component {
       <Router>
         <div>
           <SavedList list={this.state.savedList} />
-          <div>Replace this Div with your Routes</div>
+           <Route exact path="/" component={MovieList} />
+           <Route path='/movies/:id' component={Movie} />
         </div>
       </Router>
     );
